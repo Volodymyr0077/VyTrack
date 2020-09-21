87986633abf53455268227ae55bbd772887730fe
Volodymyr0077 <v.pisarzhevsky@gmail.com>
2020-09-21 16:19:28 -0500

    Test push

---
+++ src/test/java/VyTrack/B2088/AC4_CO.java
@@ -0,0 +1,13 @@
+package VyTrack.B2088;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AC4_CO {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://qa2.vytrack.com/");
+        System.out.println("Hedddllo");
+        System.out.println("test");
+    }
+}
