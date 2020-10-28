3037a3aed2abaacd87fbc28675d9217e99133c68
Volodymyr0077 <v.pisarzhevsky@gmail.com>
2020-10-27 19:30:29 -0500

    FuzzBuzz

---
+++ FuzzBuzzTask.java
@@ -0,0 +1,15 @@
+public class{
+	public static void main(String[] args) {
+		for(int i=1;i<=15;i++){
+			if(i%3==0){
+				System.out.println("Fuzz")
+				continue;
+			}
+			else if(i%5==0){
+				System.out.println("Buzz")
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+}
